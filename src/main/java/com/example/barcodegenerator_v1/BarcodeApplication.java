e35988b1703053b55106ffda5332b42f66450745
NevenDavidovic <nenoronnie@gmmail.com>
2024-06-08 11:41:00 +0200

    veca scena

--- src/main/java/com/example/barcodegenerator_v1/BarcodeApplication.java
+++ src/main/java/com/example/barcodegenerator_v1/BarcodeApplication.java
@@ -11,7 +11,7 @@ public class BarcodeApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(BarcodeApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1100, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
